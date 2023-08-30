d6e0e21caa3722640f3a1c645f92560c4597e3b5
WEI TSE <curry900312@gmail.com>
2023-08-30 11:05:07 +0800

    change baseUrl to "./src"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { api } from "@/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import Navbar from "@/components/global/navbar/Navbar";
 
 
 const MyApp: AppType = ({ Component, pageProps }: AppProps) => {

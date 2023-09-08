956667140e912ad35c22a861f8f4b9ea61736ed9
WEI TSE <curry900312@gmail.com>
2023-09-08 15:10:33 +0800

    test

--- src/pages/keyword/index.tsx
+++ src/pages/keyword/index.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/global/Navbars/Navbar";
 import DashboardSection from "@/components/page/keyword/dashboardSection/DashboardSection";
-import TableSection from "@/components/page/keyword/dataTableSection/TableSection";
+import TableSection from "@/components/page/keyword/tableSection/TableSection";
 import DataRowSelectionContextProvider from "@/context/keyword/DataRowSelectionContext";
 
 const KeywordPage = () => {

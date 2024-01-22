3f5edeb648e5565cad34cdbef18db50721f5ef89
王惟澤 <wangweize@wangweizedeMacBook-Air.local>
2024-01-22 20:31:24 +0800

    add filter function in hotspot page

--- src/components/page/keyword/tableSection/TableInputSection/TableInputForm.tsx
+++ src/components/page/keyword/tableSection/TableInputSection/TableInputForm.tsx
@@ -8,100 +8,106 @@ const HOTSPOT_DATA = [
   {
     title: "即時熱點 考拉新媒體導航",
     iframeUrl: "https://www.kaolamedia.com/hot",
-    filter_by: ["找熱門"],
+    filter_by: ["即時熱點排行"],
   },
   {
     title: "大數據詞雲 快推易",
     iframeUrl: "https://www.kt1.com/",
-    filter_by: ["找熱門"],
+    filter_by: ["大數據詞雲"],
   },
   {
     title: "熱門影片 梅花網排行",
     iframeUrl: "https://www.meihua.info/top",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門影片排行"],
   },
   {
-    title: "熱門創作者 千瓜數據熱門創作者",
+    title: "熱門創作者 千瓜數據",
     iframeUrl: "https://www.qian-gua.com/rank/fans/",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門創作者排行", "小紅書"],
   },
   {
     title: "Tiktok熱門歌曲 Countik",
     iframeUrl: "https://countik.com/zh/popular/songs",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門歌曲排行", "Tiktok"],
   },
   {
     title: "Tiktok熱門創作者 Countik",
     iframeUrl: "https://countik.com/zh/popular/creators",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門創作者排行", "Tiktok"],
   },
   {
     title: "Tiktok熱門話題 Countik",
     iframeUrl: "https://countik.com/zh/popular/topics",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門話題排行", "Tiktok"],
   },
   {
     title: "Instagram熱門創作者 SocialBook",
     iframeUrl:
       "https://socialbook.io/instagram-channel-rank/top-100-instagrammers-from-united-states",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門創作者排行", "Instagram"],
   },
   {
     title: "Youtube熱門頻道 SocialBook",
     iframeUrl:
       "https://socialbook.io/youtube-channel-rank/top-100-youtubers?sort=followers",
-    filter_by: ["找熱門"],
+    filter_by: ["熱門創作者排行", "Youtube"],
   },
   {
     title: "滴答狗 Tiktok",
     iframeUrl: "https://didadog.com/libraryProduct/productHotList",
-    filter_by: ["找社群"],
+    filter_by: ["Tiktok"],
   },
   {
     title: "新紅 小紅書",
     iframeUrl: "https://xh.newrank.cn/?source=10000&l=t_ys_sjfu_dspsj_xh",
-    filter_by: ["找社群"],
+    filter_by: ["小紅書"],
   },
 ];
 
-const FILTER_DATA = [
+const HOT_FILTER_DATA = [
   {
-    key: "找熱門",
-    data: [
+    keys: [
       {
-        title: "即時熱點排行",
+        key: "即時熱點排行",
       },
       {
-        title: "大數據詞雲",
+        key: "熱門影片排行",
       },
       {
-        title: "熱門影片排行",
+        key: "熱門歌曲排行",
       },
       {
-        title: "熱門歌曲排行",
+        key: "熱門創作者排行",
       },
       {
-        title: "熱門創作者排行",
-      },
-      {
-        title: "熱門話題排行",
+        key: "熱門話題排行",
       },
     ],
   },
+];
+const SOCIALMEDIA_FILTER_DATA = [
   {
-    key: "找社群",
-    data: [
+    keys: [
       {
-        title: "Tiktok",
+        key: "Tiktok",
       },
       {
-        title: "Instagram",
+        key: "Instagram",
       },
       {
-        title: "Youtube",
+        key: "Youtube",
       },
       {
-        title: "小紅書",
+        key: "小紅書",
+      },
+    ],
+  },
+];
+const TOOL_FILTER_DATA = [
+  {
+    keys: [
+      {
+        key: "大數據詞雲",
       },
     ],
   },
@@ -112,34 +118,85 @@ type TableInputSectionProps = {
 
 const TableInputForm = ({ setIframeUrl }: TableInputSectionProps) => {
   return (
-    <div className="w-full items-center gap-4">
-      <Tabs defaultValue="找熱門" className="w-auto">
+    <div className="w-full flex flex-col gap-4">
+      <Tabs defaultValue="即時熱點排行" className="w-auto">
         <TabsList>
-          {FILTER_DATA.map((item) => {
-            return <TabsTrigger value={item.key}>{item.key}</TabsTrigger>;
-          })}
+          {HOT_FILTER_DATA.map((keysObect) =>
+            keysObect.keys.map((key) => (
+              <TabsTrigger value={key.key} key={key.key}>
+                {key.key}
+              </TabsTrigger>
+            ))
+          )}
         </TabsList>
-        {FILTER_DATA.map((item) => {
-          return (
+        <div className="flex items-center gap-2">
+          {HOTSPOT_DATA.map((data) => (
             <TabsContent
-              value={item.key}
-              className="w-[600px] flex items-center flex-wrap gap-2"
+              value={data.filter_by.find((key) => key.includes("熱")) || ""}
+              key={data.title}
             >
-              {HOTSPOT_DATA.map((hotspot) => {
-                if (hotspot.filter_by.includes(item.key))
-                  return (
-                    <Button
-                      variant={"outline"}
-                      onClick={() => setIframeUrl(hotspot.iframeUrl)}
-                    >
-                      {hotspot.title}
-                    </Button>
-                  );
-                return null;
-              })}
+              <Button
+                variant={"default"}
+                onClick={() => setIframeUrl(data.iframeUrl)}
+              >
+                {data.title}
+              </Button>
             </TabsContent>
-          );
-        })}
+          ))}
+        </div>
+      </Tabs>
+
+      <Tabs defaultValue="Tiktok" className="w-auto">
+        <TabsList>
+          {SOCIALMEDIA_FILTER_DATA.map((keysObect) =>
+            keysObect.keys.map((key) => (
+              <TabsTrigger value={key.key} key={key.key}>
+                {key.key}
+              </TabsTrigger>
+            ))
+          )}
+        </TabsList>
+        <div className="flex items-center gap-2">
+          {HOTSPOT_DATA.map((data) => (
+            <TabsContent
+              value={data.filter_by.find((key) => !key.includes("熱")) || ""}
+              key={data.title}
+            >
+              <Button
+                variant={"default"}
+                onClick={() => setIframeUrl(data.iframeUrl)}
+              >
+                {data.title}
+              </Button>
+            </TabsContent>
+          ))}
+        </div>
+      </Tabs>
+      <Tabs defaultValue="大數據詞雲" className="w-auto">
+        <TabsList>
+          {TOOL_FILTER_DATA.map((keysObect) =>
+            keysObect.keys.map((key) => (
+              <TabsTrigger value={key.key} key={key.key}>
+                {key.key}
+              </TabsTrigger>
+            ))
+          )}
+        </TabsList>
+        <div className="flex items-center gap-2">
+          {HOTSPOT_DATA.map((data) => (
+            <TabsContent
+              value={data.filter_by.find((key) => !key.includes("熱")) || ""}
+              key={data.title}
+            >
+              <Button
+                variant={"default"}
+                onClick={() => setIframeUrl(data.iframeUrl)}
+              >
+                {data.title}
+              </Button>
+            </TabsContent>
+          ))}
+        </div>
       </Tabs>
 
       <p className="text-xs text-gray-500 mt-5">

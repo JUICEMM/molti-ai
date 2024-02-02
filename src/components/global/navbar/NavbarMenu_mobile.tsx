ecb9c770b6ee4f05d24fb374bce28c6256e3fbf7
王惟澤 <wangweize@wangweizedeMacBook-Air.local>
2024-02-02 12:08:14 +0800

    add consultant features

--- src/components/global/navbar/NavbarMenu_mobile.tsx
+++ src/components/global/navbar/NavbarMenu_mobile.tsx
@@ -1,14 +1,14 @@
-import { MenuIcon } from "lucide-react";
-import Link from "next/link";
 import CloseIcon from "@mui/icons-material/Close";
 import { IconButton } from "@mui/material";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
 
 type NavbarMenu_mobileProps = {
   navItems: Array<{ title: string; href: string }>;
-}
+};
 
-const NavbarMenu_mobile = ({navItems}: NavbarMenu_mobileProps) => {
+const NavbarMenu_mobile = ({ navItems }: NavbarMenu_mobileProps) => {
   const [isMobileNavbarMenuOpen, setIsMobileNavbarMenuOpen] =
     useState<boolean>(false);
   return (
@@ -17,15 +17,15 @@ const NavbarMenu_mobile = ({navItems}: NavbarMenu_mobileProps) => {
         <MenuIcon />
       </button>
       <div
-        className={
+        className={`${
           isMobileNavbarMenuOpen === true
-            ? "fixed inset-0  z-[999] h-screen bg-neutral-200 bg-opacity-90 backdrop:blur"
+            ? "fixed inset-0  z-[999] max-h-screen bg-neutral-200 bg-opacity-90 backdrop:blur"
             : "fixed z-[999]  hidden h-0"
-        }
+        } transition-all duration-200 ease-in-out`}
       >
         <div className="absolute top-5 right-5">
           <IconButton onClick={() => setIsMobileNavbarMenuOpen(false)}>
-            <CloseIcon sx={{ color: "black",fontSize: "2rem" }} />
+            <CloseIcon sx={{ color: "black", fontSize: "2rem" }} />
           </IconButton>
         </div>
         <div className="flex h-screen items-center justify-center">
--- src/pages/consultant/index.tsx
+++ src/pages/consultant/index.tsx
@@ -1,5 +1,25 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import EmergencyRecordingOutlinedIcon from '@mui/icons-material/EmergencyRecordingOutlined';
+
+const consultantMap = [
+  {
+    icon: <LanguageOutlinedIcon sx={{ color: "black", fontSize: "3.2rem" }} />,
+    title: "Tiktok代運營",
+    subtitle: ["開設Tiktok帳號", "讓你快速漲粉", "鎖定您的專屬紛絲"],
+  },
+  {
+    icon: <PeopleOutlinedIcon sx={{ color: "black", fontSize: "3.2rem" }} />,
+    title: "Tiktok經營教學",
+    subtitle: ["養帳號秘訣", "如何尋找您的粉絲", "經營方向建議"],
+  },
+  {
+    icon: <EmergencyRecordingOutlinedIcon sx={{ color: "black", fontSize: "3.2rem" }} />,
+    title: "Tiktok短影音拍攝",
+    subtitle: ["企業主題企劃", "抓緊爆紅內容", "鼓勵粉絲與品牌戶互動，病毒式傳播效益"],
+  },
+];
 
 export default function ConsultantPage() {
   return (
@@ -19,31 +39,46 @@ export default function ConsultantPage() {
 
         {/* 兩個小Section */}
       </div>
-      {/* <div className="container mx-auto p-8 rounded-md text-gray-800">
-        <div className=" mb-8 lg:mb-0 p-6 rounded-md bg-white">
-          <h2 className="text-2xl font-bold mb-4">有遇到以下問題？</h2>
-          <div className="flex justify-center items-center">
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
-          </div>
-        </div>
-
+      <div className="container mx-auto p-8 rounded-md text-gray-800">
         <div className="p-6 rounded-md bg-white mt-5">
           <h2 className="text-2xl font-bold mb-4">我們的優勢</h2>
-          <div className="grid grid-cols-2">
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
-            <div className="p-2 shadow-lg text-xl rounded-md">123</div>
+          <div className="grid grid-cols-1 md:grid-cols-3 justify-items-center gap-5">
+            {consultantMap.map((item, index) => (
+              <ConsultantCard key={index} {...item} index={index} />
+            ))}
           </div>
         </div>
-      </div> */}
+      </div>
       <div className="py-2 text-center">
         <Button className="px-6 py-8">
-          <a href="https://lin.ee/LciUsSk" className="px-6 py-8">立即聯絡</a>
+          <a href="https://lin.ee/LciUsSk" className="px-6 py-8">
+            立即聯絡
+          </a>
         </Button>
       </div>
     </>
   );
 }
+
+type ConsultantCardProps = {
+  icon: JSX.Element;
+  title: string;
+  subtitle: string[];
+  index: number;
+};
+
+const ConsultantCard = ({ icon, title, subtitle, index}: ConsultantCardProps) => {
+  return (
+    <div className="p-4 flex flex-col items-center gap-3 w-[300px]">
+      <div>{icon}</div>
+      <h2 className={`text-2xl font-bold text-blue-500`}>{title}</h2>
+      <ul className="list-disc list-inside">
+        {subtitle.map((item, index) => (
+          <li key={index} className="mb-2">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
